9425ef3e637a524afef7fb52a5da92f7e547e22d
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2023-05-14 22:11:45 -0400

    Axios

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -33,6 +33,39 @@ const AppContext = React.createContext()
 const AppProvider = ({children}) =>{
     const [state,dispatch] = useReducer(reducer, initialState)
 
+// axios
+
+const authFetch = axios.create({
+    baseURL: '/api/v1',
+})
+
+// request
+
+authFetch.interceptors.request.use(
+    (config) => {
+      config.headers['Authorization'] = `Bearer ${state.token}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  // response
+  authFetch.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      console.log(error.response);
+      if (error.response.status === 401) {
+        console.log('AUTH ERROR');
+      }
+      return Promise.reject(error);
+    }
+  );
+  
+
 const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT})
     clearAlert()
@@ -85,7 +118,11 @@ const removeUserToLocalStorage =()  =>{
     }
 
     const updateUser = async (currentUser) => {
-        console.log(currentUser)
+        try {
+            const { data } = await authFetch.patch('/auth/updateUser', currentUser)
+        } catch (error) {
+            console.log(error.response)
+        }
     }
 
     return (
