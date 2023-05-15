b0b9bfb64b3c6fb41fc83c502bf7de593050f02e
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2023-05-15 18:49:31 -0400

    Job Model and Create Job controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,19 @@
-const createJob = async(req, res) => {
-    res.send('create job')
-} 
+import Job from '../models/Job.js'
+import {StatusCodes} from 'http-status-codes'
+import {BadRequestError, UnAuthenticatedError} from '../errors/index.js'
+
+const createJob = async (req, res) => {
+    const { position, company } = req.body;
+  
+    if (!position || !company) {
+      throw new BadRequestError('Please Provide All Values');
+    }
+  
+    req.body.createdBy = req.user.userId;
+  
+    const job = await Job.create(req.body);
+    res.status(StatusCodes.CREATED).json({ job });
+  };
 const getAllJobs = async(req, res) => {
     res.send('got all job')
 } 
